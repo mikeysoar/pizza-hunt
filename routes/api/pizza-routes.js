8283b0292332b4de4193d1628fc3809beace530b
Mikeysoar <mwclimb@gmail.com>
2021-08-19 16:13:41 -0600

    added get, create, put, post and delete. can add a pizza from url. 20210819-1613

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+// set up GET all POST at /api/pizzes
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+// Set up GET one, PUT, and Delete at /api/pizzas/:id
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+module.exports = router;
