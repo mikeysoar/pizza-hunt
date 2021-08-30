2bc7f3f37c1e4a6fa0da2fec8cf791e622857577
Mikeysoar <mwclimb@gmail.com>
2021-08-29 21:23:49 -0600

    added populate to get all and getPizzaById. 20210829-2123

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,9 +1,16 @@
 const { Pizza } = require('../models');
+const { populate } = require('../models/Pizza');
 
 const pizzaController = {
     // Get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                Select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -14,10 +21,21 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-            .then(dbPizzaData => res.json(dbPizzaData))
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
             .catch(err => {
                 console.log(err);
-                res.sendStatus(400);
+                res.status(400).json(err);
             });
     },
 
